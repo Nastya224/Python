5b2f5a7e6b5afdc0fd01551f5ac9a1d5990cdf5e
Nastya_keks <aagolunova@edu.hse.ru>
2022-12-17 19:27:42 +0300

    first

---
+++ task64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите натуральное число больше 0:");
+int N = int.Parse(Console.ReadLine());
+ 
+void NumberCounter (int N)
+{
+    if (N == 0) return;
+    Console.Write("{0,4}", N);
+    NumberCounter (N - 1);
+}
+ 
+NumberCounter(N);
