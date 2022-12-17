3093b8d58baf8f59967432baa3797fa09e5fc5a3
Nastya_keks <aagolunova@edu.hse.ru>
2022-12-17 19:28:59 +0300

    second

---
+++ task66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите натуральное число больше нуля M:");
+int M = int.Parse(Console.ReadLine());
+ 
+Console.WriteLine("Введите натуральное число больше нуля N:");
+int N = int.Parse(Console.ReadLine());
+ 
+void GaPSum (int M, int N, int sum)
+{
+    if (M > N)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (M++);
+    GaPSum(M, N, sum);
+}
+ 
+GaPSum(M, N, 0);
